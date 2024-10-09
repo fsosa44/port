4da4d597752aa49217dd4c6376e5bbf9efd9e6a9
fsosa44 <sosafrancisco2005@gmail.com>
2024-10-09 12:12:26 -0300

    portfolio improved

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,25 +1,31 @@
 import React from 'react';
-import '../styles/navbar.css'; // Asegúrate de crear este archivo CSS
+import '../styles/navbar.css'; 
 
 const Navbar = () => {
 
-  const scrollToSection = (sectionId) => {
+  const scrollToSection = (sectionId, offset = 0) => {
     const section = document.getElementById(sectionId);
     if (section) {
-      section.scrollIntoView({ behavior: 'smooth' });
+      const sectionPosition = section.getBoundingClientRect().top + window.pageYOffset;
+      const offsetPosition = sectionPosition - offset; 
+
+      window.scrollTo({
+        top: offsetPosition,
+        behavior: 'smooth',
+      });
     }
   };
 
   return (
     <nav className="navbar">
       <div className="navbar-logo">
-        {/* Aquí puedes poner tu logo si tienes uno */}
+   
       </div>
       <ul className="navbar-menu">
-        <li className="navbar-item" onClick={() => scrollToSection('h1-about')}>Sobre mí</li>
-        <li className="navbar-item" onClick={() => scrollToSection('projects-section')}>Proyectos</li>
-        <li className="navbar-item" onClick={() => scrollToSection('skills-section')}>Skills</li>
-        <li className="navbar-item" onClick={() => scrollToSection('contact-section')}>Contacto</li>
+        <li className="navbar-item" onClick={() => scrollToSection('h1-about', 660)}>Sobre mí</li>
+        <li className="navbar-item" onClick={() => scrollToSection('projects-section', 100)}>Proyectos</li>
+        <li className="navbar-item" onClick={() => scrollToSection('skills-section', 100)}>Skills</li>
+      
       </ul>
     </nav>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,21 @@
 import React from 'react';
+
 import { FaGithub, FaLinkedin } from 'react-icons/fa';
 import profilePic from '../styles/profilePic.png'; 
+import project1 from '../styles/houseofdev.png'
+import project2 from '../styles/fastdelivery.png'
+import project3 from '../styles/barcaGame.png'
 import '../styles/home.css'; 
+
+import JavaScriptLogo from '../styles/JavaScript-logo.png'
+import ReactLogo from '../styles/react-logo.png'
+import NextLogo from '../styles/next-logo.png'
+import NodeLogo from '../styles/node-logo.png'
+import PostgresLogo from '../styles/postgres-logo.png'
+import sequelizeLogo from '../styles/sequelize-logo.png'
+import ReduxLogo from '../styles/redux-logo.png'
+import cssLogo from '../styles/css-logo.png'
+import htmlLogo from '../styles/html-logo.png'
 
 const Home = () => {
     return (
@@ -16,8 +30,10 @@ const Home = () => {
             <h2 className="position">Desarrollador Full Stack</h2>
     
             <div className="buttons">
-              <button className="button button-outline">Descargar CV</button>
-              <button className="button">Información de contacto</button>
+            <a href="/CV_Francisco_Sosa.pdf" download>
+    <button className="button button-outline">Descargar CV</button>
+  </a>
+            
             </div>
     
             <div className="icons">
@@ -31,28 +47,83 @@ const Home = () => {
           </div>
         </div>
 
-        {/* Contenedor para las secciones en dos columnas */}
+       
         <div className="container-columns">
-          {/* Sección "Acerca de mí" */}
+         
           <div id="about-section" className="about-section">
             <h2 id='h1-about'>Acerca de mí</h2>
             <hr className="divider" />
             <p>
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vero quia blanditiis tenetur similique. Aspernatur alias fugiat doloremque rem culpa dolorum nobis odit perspiciatis sunt eum adipisci architecto, ipsa reiciendis quod! Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates magni iste molestiae velit possimus deleniti facere voluptatibus quidem ut sint eveniet aliquam labore dicta omnis dolorum, laudantium eos tenetur expedita?
+            Soy una persona proactiva, comprometida con el aprendizaje continuo y la mejora de mis habilidades. Destaco por mis sólidas capacidades de comunicación y mi entusiasmo por colaborar en equipo. Me apasiona trabajar en conjunto con otros, ya que considero que la sinergia y el intercambio de ideas son clave para alcanzar resultados exitosos. Estoy siempre dispuesto a enfrentar nuevos desafíos y contribuir al crecimiento del equipo.
             </p>
           </div>
 
-          {/* Sección "Proyectos" */}
+         
           <div id="projects-section" className="projects-section">
             <hr className="divider" />
             <h2>Proyectos</h2>
             </div>
           </div>
             <div className="projects-images">
-              <img src="https://via.placeholder.com/150" alt="Proyecto 1" />
-              <img src="https://via.placeholder.com/150" alt="Proyecto 2" />
-              <img src="https://via.placeholder.com/150" alt="Proyecto 3" />
+            <a href="https://front-pink-beta.vercel.app/" target="_blank" rel="noopener noreferrer">
+    <img src={project1} alt="Proyecto 1" />
+  </a>
+               <a href="https://delivery-front-eight.vercel.app/" target="_blank" rel="noopener noreferrer">
+              <img src={project2} alt="Proyecto 2" />
+              </a>
+              <a href="https://gregarious-entremet-5c511e.netlify.app/" target="_blank" rel="noopener noreferrer">
+              <img src={project3} alt="Proyecto 2" />
+              </a>
+
+
+
         </div>
+        <div id="skills-section" className="skills-section">
+  <div className="skills-header">
+    <h2>Skills</h2>
+    <hr className="divider-skills" />
+  </div>
+ <div className="skills-images">
+  <div className="skill-item">
+    <img src={JavaScriptLogo} alt="Skill 1" className="js-logo" />
+    <p>JavaScript</p>
+  </div>
+  <div className="skill-item">
+    <img src={ReactLogo} alt="Skill 2" />
+    <p>React.js</p>
+  </div>
+  <div className="skill-item">
+    <img src={NextLogo} alt="Skill 3" />
+    <p>Next.js</p>
+  </div>
+  <div className="skill-item">
+    <img src={NodeLogo} alt="Skill 4" />
+    <p>Node.js</p>
+  </div>
+  <div className="skill-item">
+    <img src={PostgresLogo} alt='Skill 5' />
+    <p>PostgreSQL</p>
+  </div>
+  <div className="skill-item">
+    <img src={sequelizeLogo} alt="Skill 6" />
+    <p>Sequelize.js</p>
+  </div>
+  <div className="skill-item">
+    <img src={htmlLogo} alt="Skill 7" />
+    <p>HTML5</p>
+  </div>
+  <div className="skill-item">
+    <img src={cssLogo} alt="Skill 8" />
+    <p>CSS3</p>
+  </div>
+  <div className="skill-item">
+    <img src={ReduxLogo} alt="Skill 9" />
+    <p>Redux</p>
+  </div>
+</div>
+</div>
+
+      
       </>
     );
 };
